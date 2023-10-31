25889c728b4c5caff4e26aaba4b4fd77339c0d22
0xUgochukwu <0xUgochukwu@gmail.com>
2023-10-31 14:19:17 +0100

    handle copy

--- server/src/services/session.service.ts
+++ server/src/services/session.service.ts
@@ -1,9 +1,26 @@
 export const Socket = (io: any) => {
     io.sockets.on('connection', (socket: any) => {
         console.log(socket.id, "Connected!");
+
+        // Start Session 
         socket.on('start', (sessionID: string) => {
             socket.join(sessionID);
+            console.log(`${socket.id}, Started Session: ${sessionID}`)
+        });
+
+
+        // Join Session
+        socket.on('join', (sessionID: string) => {
+            socket.join(sessionID);
             console.log(`${socket.id}, Joined Session: ${sessionID}`)
+        });
+
+
+        // Handle copy on client
+        socket.on('copy', (sessionID: string, clip: any) => {
+            console.log(`${socket.id} in ${sessionID} Copied something: ${clip}`);
+
+            socket.to(sessionID).emit('sync', clip);
         });
     });
 }
