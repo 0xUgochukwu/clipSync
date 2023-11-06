a3258aeabd97731a5e3a1ae55f87dad6cfd1ba2e
0xUgochukwu <0xUgochukwu@gmail.com>
2023-11-06 12:44:52 +0100

    update server

--- server/src/services/session.service.ts
+++ server/src/services/session.service.ts
@@ -1,5 +1,6 @@
 export const Socket = (io: any) => {
 
+    // Authenticate Client
     io.use((socket: any, next: any) => {
         if (socket.handshake.query && socket.handshake.query.sessionID) {
             socket.sessionID = socket.handshake.query.sessionID;
@@ -17,8 +18,24 @@ export const Socket = (io: any) => {
             console.log(`${socket.id}, Joined Session: ${socket.sessionID}`)
         });
 
+        // Leave Session
+        socket.on('leave', () => {
+            socket.leave(socket.sessionID);
+            console.log(`${socket.id}, Left Session: ${socket.sessionID}`)
+        });
 
-        // Handle copy on client
+        // End Session
+        socket.on('end', () => {
+            socket.clients(socket.sessionID).forEach((client: any) => {
+                client.leave(socket.sessionID);
+                console.log(`[ Closing ] ${socket.id}, Left Session: ${socket.sessionID}`)
+            });
+
+            console.log(`Everything dies, session ${socket.sessionID} just did`);
+        });
+
+
+        // Handle copy event on client
         socket.on('copy', (clip: any) => {
             console.log(socket.sessionID);
             console.log(`${socket.id} in ${socket.sessionID} Copied something: ${clip}`);
